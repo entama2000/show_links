7107119a856fbb46758cf438820b98d79da0cc3e
entama2000 <103039176+entama2000@users.noreply.github.com>
2022-09-24 02:16:40 -0400

    add base files

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function() {
+  $("#instaID").click(function() {
+    window.location.href = "https://www.instagram.com/e.n.t.a.m.a/";
+  });
+  $("#instaBtn").click(function() {
+    window.location.href = "https://www.instagram.com/e.n.t.a.m.a/";
+  });
+  $("#fbBtn").click(function() {
+    window.location.href = "https://www.facebook.com/people/Tamaki-Endo/100009660947760/";
+  });
+  $("#liBtn").click(function() {
+    window.location.href = "https://www.linkedin.com/in/entama";
+  });
+  $("#resumeBtn").click(function() {
+    window.location.href = "/pdf/Tamaki_Endo_Resume.pdf";
+  });
+  $("#blog1Btn").click(function() {
+    window.location.href = "https://ryugaku.entama.jp/";
+  });
+  $("#wolfBtn").click(function() {
+    window.location.href = "https://entama.jp/werewolf/";
+  });
+});
